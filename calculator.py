3ffe77adce7aa0e59088239aecfb4565f81abefc
asherjallen-jpg <asherjallen@gmail.com>
2025-11-12 14:17:43 -0500

    First commit

--- calculator.py
+++ calculator.py
@@ -4,8 +4,28 @@ calculator.py
 
 One function per operation, in order.
 """
+import math
 # First example
 def add(a, b): 
-    pass
+    return a + b
 
+def sub(a, b):
+    return a - b
 
+def mul(a, b):
+    return a * b
+
+def div(a, b):
+    if a == 0:
+        raise ZeroDivisionError("Cannot divide by zero")
+    return b / a
+
+def log(a, b):
+    if a <= 1:
+        raise ValueError("Log base cannot be less than 2")
+    if b < 1:
+        raise ValueError("Cannot use log on negative numbers")
+    return math.log(b, a)
+
+def exp(a, b):
+    return a ** b
